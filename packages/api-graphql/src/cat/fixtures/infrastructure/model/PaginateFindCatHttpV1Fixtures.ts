231b2cbce1ed45074b2ce0156121409f83528803
Adrianmjim <adrianmjim199434@gmail.com>
2023-10-29 20:00:34 +0100

    WIP

--- packages/api-graphql/src/cat/fixtures/infrastructure/model/PaginateFindCatHttpV1Fixtures.ts
+++
@@ -1,9 +0,0 @@
-import { PaginateFindCatHttpV1 } from '../../../infrastructure/http/model/PaginateFindCatHttpV1';
-
-export class PaginateFindCatHttpV1Fixtures {
-  public static get any(): PaginateFindCatHttpV1 {
-    const paginateFindCatHttpV1: PaginateFindCatHttpV1 = new PaginateFindCatHttpV1();
-
-    return paginateFindCatHttpV1;
-  }
-}
--- packages/api-graphql/src/cat/fixtures/infrastructure/model/UpdateOneCatHttpV1Fixtures.ts
+++
@@ -1,9 +0,0 @@
-import { UpdateOneCatHttpV1 } from '../../../infrastructure/http/model/UpdateOneCatHttpV1';
-
-export class UpdateOneCatHttpV1Fixtures {
-  public static get any(): UpdateOneCatHttpV1 {
-    const updateOneCatHttpV1: UpdateOneCatHttpV1 = new UpdateOneCatHttpV1();
-
-    return updateOneCatHttpV1;
-  }
-}
